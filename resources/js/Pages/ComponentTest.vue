cc05d46693db69a717a0993ccfe579ee2a596a5d
tokuyama_shota <tokush1031@gmail.com>
2023-10-30 13:57:35 +0900

    vue3

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,17 @@
+<script setup>
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import TextInput from "@/Components/TextInput.vue";
+
+const emitTest = (e) => console.log(e);
+</script>
+
+<template>
+    <GuestLayout>
+        <InputLabel>タイトル</InputLabel>
+        <TextInput
+            modelValue="初期値です。"
+            @update:modelValue="emitTest"
+        ></TextInput>
+    </GuestLayout>
+</template>
--- resources/js/Pages/Inertia/Show.vue
+++ resources/js/Pages/Inertia/Show.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
+import { Inertia } from "@inertiajs/inertia";
+
 defineProps({
     id: String,
+    blog: Object,
 });
+
+const deleteConfirm = (id) => {
+    console.log(id);
+    Inertia.delete(`/inertia/${id}`, {
+        onBefore: () => confirm("本当に削除しますか？"),
+    });
+};
 </script>
 <template>
-    {{ id }}
+    {{ id }}<br />
+    {{ blog.title }}<br />
+    <button @click="deleteConfirm(blog.id)">削除</button>
 </template>
